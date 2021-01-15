af7433447bc1653cc8c47e93aeda6406759f13dd
NAVTTC07 <numrah.alauddin@gmail.com>
2021-01-15 18:10:49 +0500

    Multiple logical Operators

--- src/logicaloperators/LogicalOperators.java
+++ src/logicaloperators/LogicalOperators.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package logicaloperators;
 
 import java.util.Scanner;
 
-/**
- *
- * @author NAVTTC07
- */
 public class LogicalOperators {
 
     public static void main(String[] args) {
-        
+     
         //ANd OR Not---> && || !
         //ANd --> T and T = T
                    //T and F =F
@@ -31,24 +23,72 @@ public class LogicalOperators {
             System.out.println("Grade C");
         }
         
+//        Scanner user=new Scanner(System.in);
+//        
+//        System.out.println("Enter a or b");
+//        char ch=user.next().charAt(0);
+        
+        
+//        if(ch=='y' || ch=='Y'){
+//            System.out.println("OK Continue");
+//        }
+//        
+//        else
+//        {
+//            System.out.println("Exit");
+//        }
+     
+//        if((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') || (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')){
+//            System.out.println("Vowel");
+//        }
+//        else
+//        { 
+//            System.out.println("InCorrect");
+//        }
+       
+        //a,e,i,o,u
+        
+//        System.out.println("Enter any character");
+//        char ch1=user.next().charAt(0);
+//        
+//        //ch1 >='a' || ch1>='A' && ch1<='z' || ch1<='Z'
+//        
+//        if((ch1 >='a' && ch1<='z') || (ch1 >='A' && ch1<='Z')) {
+//            System.out.println("Alphabet");
+//        }
+//        
+//        else if(ch1 >='0' && ch1<='9'){
+//            System.out.println("Digits");
+//        }
+//        else
+//        {
+//            
+//            System.out.println("Symbol");
+//        }
+//        
         Scanner user=new Scanner(System.in);
+        char ch2=65;
+        System.out.println("ch2 "+ch2);
         
-        System.out.println("Do you want to continue y/n");
-        char ch=user.next().charAt(0);
+        System.out.println("Enter any integer");
+       // char ch3=user.next().charAt(0);
+       int ch3=user.nextInt();//45
+       ch3=ch3+45;
         
+        //System.out.println("ch3 "+ch3);
         
-        if(ch=='y' || ch=='Y'){
-            System.out.println("OK Continue");
+         if((ch3 >=97 && ch3<= 122) || (ch3 >=65 && ch3<=90)) {
+            System.out.println("Alphabet");
         }
         
+        else if(ch3 >=47 && ch3<=57){
+            System.out.println("Digits");
+        }
         else
         {
-            System.out.println("Exit");
+            
+            System.out.println("Symbol");
         }
-        user.nextLine();
-        System.out.println("Enter any string");
-        String word=user.nextLine();
-        System.out.println("word "+word);
         
     }
     
